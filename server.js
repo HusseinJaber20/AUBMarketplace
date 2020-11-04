9999582ef9506deda11394fdb0c2544c1517df91
Hussein <jaberhussein208@gmail.com>
2020-11-04 22:25:10 +0200

    adding to github

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
 
-app.get('/', (req,res) => res.send('API Running :)'))
+// Connect Database
+connectDB();
+
+// localhost:5000
+app.get('/', (req,res) => res.send('API Running :)'));
+
+// Define Routes
+app.use('/api/users' , require('./routes/api/users'));
+app.use('/api/auth' , require('./routes/api/auth'));
+app.use('/api/profiles' , require('./routes/api/profiles'));
+app.use('/api/posts' , require('./routes/api/posts'));
+
 
 const PORT = process.env.PORT || 5000;
 
