d8d0f62e6921215778719539f0281aca16b5cce1
Hussein <jaberhussein208@gmail.com>
2020-11-05 21:43:58 +0200

    Created the User Schema, and the api of registering a user to the database

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+// Each Schema in mongoose maps to a MongoDB collection and defines
+// the shape of the documents within that collection.
+// A collection in a NoSQL (MongoDB) is analogous to a table of an RDBMS
+// A document in a collection is analogous to a row in a table of an RDBMS
+
+// Link : https://mongoosejs.com/docs/guide.html
+
+const UserSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        require : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    password: {
+        type : String,
+        required : true
+    }
+});
+
+// You can think of a model as a constructor. It will be used too to query from the DB.
+module.exports = User = mongoose.model('users', UserSchema);
