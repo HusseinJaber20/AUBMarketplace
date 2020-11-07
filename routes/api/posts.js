d728302cbc7e2b573272927a81f77bc9e01a51a9
Hussein <jaberhussein208@gmail.com>
2020-11-07 22:42:34 +0200

    A user can now push a prdouct into the database

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,41 @@
 const express = require('express')
 const router = express.Router();
+const Product = require('../../models/Product')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const auth = require('../../middleware/auth')
+const {check, validationResult} = require('express-validator/check')
 
-// @route  GET api/posts
-// @desc   Test route
+// @route  Post api/posts
+// @desc   Post about a product
 // @acess  Public
-router.get('/', (req,res) => res.send('Post route'));
+
+router.post('/', auth, [
+    check('name','Name is empty').not().isEmpty(),
+    check('description','Description is empty').not().isEmpty(),
+    check('price','price is empty').not().isEmpty()
+], async (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+    const {name, category, price, description, status, images} = req.body;
+    try{
+        product = new Product ({
+            name,
+            category,
+            price,
+            description,
+            seller : req.user.id,
+            status,
+            images,
+        })
+        await product.save();
+        res.json({ msg : "Product saved successfully!"})
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
+});
 
 module.exports = router;
