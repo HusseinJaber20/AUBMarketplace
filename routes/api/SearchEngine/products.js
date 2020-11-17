c1f31e0dbfe59c4ed61b0a9f657edb25e2e07233
Hussein <jaberhussein208@gmail.com>
2020-11-17 22:50:05 +0200

    First step of Search Engine..

--- routes/api/SearchEngine/products.js
+++ routes/api/SearchEngine/products.js
@@ -1,7 +1,29 @@
 const express = require('express')
-const Product= require('../../../models/Product')
+const Product = require('../../../models/Product')
 const auth = require('../../../middleware/auth')
 
 const router = express.Router();
+
+router.get('/', auth, async (req,res) => {
+    try {
+        let result = await Product.aggregate([
+            {
+                "$search": {
+                    "autocomplete": {
+                        "query": `${req.query.query}`,
+                        "path": "name",
+                        "fuzzy": {
+                            "maxEdits": 2,
+                            "prefixLength": 3
+                        }
+                    }
+                }
+            }
+        ])
+        res.send(result);
+    } catch (e) {
+        res.status(500).send({ message: e.message });
+    }
+})
 
 module.exports = router;
