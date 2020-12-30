18cf4ebd39b86fa8c62046f0ead74ff560a63f99
Hussein <jaberhussein208@gmail.com>
2020-12-30 23:42:59 +0200

    Buying and selling product started with

--- emails/account.js
+++ emails/account.js
@@ -46,7 +46,29 @@ const sendCancellationEmail = (email, name) => {
     })
 }
 
+//Email sent once a new user signs up
+const sendRequestEmail = (email, seller, buyer) => {
+    sgMail.send({
+        to: email,
+        from: process.env.SENDGRID_SENDER_EMAIL,
+        subject: 'Someone wants your item!',
+        html: `
+        <div>Someone wants your item!!</div>
+        <div>&nbsp;</div>
+        <div>Hello, ${seller}, we&#39;re happy&nbsp;to inform you that ${buyer} wants to buy your item!!</div>
+        <div>Please check the application for more information!</div>
+        <div>&nbsp;</div>
+        <div>Best regards,</div>
+        <div>AUB Marketplace Team</div>
+        `
+    }).catch((e)=>{
+        console.log(e)
+    })
+}
+
+
 module.exports = {
     sendWelcomeEmail,
-    sendCancellationEmail
+    sendCancellationEmail,
+    sendRequestEmail
 }
--- routes/api/products.js
+++ routes/api/products.js
@@ -4,6 +4,7 @@ const Product = require('../../models/Product')
 const User = require('../../models/User')
 const auth = require('../../middleware/auth')
 const {check, validationResult} = require('express-validator')
+const {sendRequestEmail} = require('../../emails/account')
 ObjectId = require('mongodb').ObjectID;
 
 
@@ -89,6 +90,60 @@ router.patch('/:id', auth, async (req, res) => {
     }
 })
 
+//Request to buy a product 
+router.patch('/buy/:id', auth, async(req,res) => {
+    try{
+        const product = await Product.findById(req.params.id)
+        if(!product){
+            return res.status(400).send({err: 'Product not found with such an id'})
+        }
+        if(product.owner == req.user.id || product.status!='Available'){
+            return res.status(400).send({err: 'You can not buy this product'})
+        }
+        product.status = 'Pending'
+        await product.save()
+        const seller = await User.findById(product.owner)
+        const buyer = await User.findById(req.user.id)
+        sendRequestEmail(seller.email, seller.name, buyer.name)
+        return res.status(201).json({Message: 'Request sent!'})
+    } catch(err){
+        return res.status(401).send({err})
+    }
+})
+
+// Product sold after agreement with the buyer
+router.patch('/sell/:id', auth, async(req,res) => {
+    try{
+        const product = await Product.findOne({_id:req.params.id, owner: req.user.id})
+        if(!product){
+            return res.status(401).send({Message : 'You have no such product'})
+        }
+        product.status = 'Sold'
+        await product.save()
+        return res.status(201).json({Message : 'Status Changed'})
+    } catch(err){
+        return res.status(401).send(err)
+    }
+})
+
+// Rejects a request and makes the product available again
+router.patch('/reject/:id', auth, async(req,res) => {
+    try{
+        const product = await Product.findOne({_id:req.params.id, owner: req.user.id})
+        if(!product){
+            return res.status(401).send({Message : 'You have no such product'})
+        }
+        if(product.status!= 'Pending'){
+            return res.status(201).json({Message : 'Product already not pending!'})
+        }
+        product.status = 'Available'
+        await product.save()
+        return res.status(201).json({Message : 'Status Changed'})
+    } catch(err){
+        return res.status(401).send(err)
+    }
+})
+
 // Delete Product  
 router.delete('/:id', auth, async (req,res) => {
     try {
@@ -103,5 +158,6 @@ router.delete('/:id', auth, async (req,res) => {
         res.status(500).send('Server Error')
     }
 });
+
 
 module.exports = router;
